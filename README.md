8db911b4e054de88cdb094eb2fcf59f8f0b99b77
Michael M <mmm111@hotmail.com>
2022-06-16 17:03:49 -0500

    changes

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # pya-web3
+
+1) Create Virtual environment [Windows]
+    # python3 -m env <name_of_virtualenv>
+  #  python -m venv ./env
+
+2) See struture of virtual environment
+  # tree env
+
+3) Activate Virtual Environment
+
+ # .\env\Scripts\activate
+
+4) Install python packages in virtual environment
+ # pip install flask web3 
+
+6) Create requirements.txt
+
+# pip3 freeze > requirements.txt 
+ 
+6) Run the app
+ 
+python app.py
+
+# PYTHON VERSION python --version 
+Python 3.10.4
---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from web3 import Web3
+
+
+app = Flask(__name__)
+app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
+app.config['SECURITY_PASSWORD_SALT']='thisistheSALTforcreatingtheCONFIRMATIONtoken'
+app.config['ETH_INFURA_MAINNET_URL'] = 'https://mainnet.infura.io/v3/47d1365cc1d94d70b061dc255574a787' 
+
+address =  Web3.toChecksumAddress("0xea674fdde714fd979de3edf0f56aa9716b898ec8")
+
+web3 = Web3(Web3.HTTPProvider(app.config['ETH_INFURA_MAINNET_URL']))
+
+@app.route('/')
+def index():
+
+    is_connected = web3.isConnected()
+    latestBlock = web3.eth.blockNumber
+    balance = web3.eth.getBalance(address)
+    balance = web3.fromWei(balance, "ether")
+
+    return render_template('index.html', data ={"isConnected" : is_connected, "latestBlock" : latestBlock , "address" : address, "balance" : balance })
+
+
+if __name__ == '__main__':
+
+   app.run(host='127.0.0.1',port=8889,debug=True)
